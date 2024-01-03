877ea44dea9bb66d29439f9b3d00eb749358524e
Dener <Dener@DESKTOP-VD0LQHS>
2024-01-03 16:02:51 +0000

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeControllers.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+using Todo.Models;
+
+namespace Todo.Controllers
+{
+    [ApiController]
+    public class HomeControllers : ControllerBase
+    {
+        [HttpGet("/")]
+        public List<TodoModel> Get( [FromServices] AppDbContext context)
+        {
+            return context.Todos.ToList();
+        }
+        
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
